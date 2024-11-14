a2c4423b9fea89ea2b3559b98e0f3184d3ee2129
Java2112 <84025096+Java2112@users.noreply.github.com>
2024-11-13 20:12:52 -0500

    xd error mk

--- src/main/java/co/edu/personasapi/domain/Producto.java
+++ src/main/java/co/edu/personasapi/domain/Producto.java
@@ -23,9 +23,6 @@ public class Producto {
     private String nombre;
 
     @Column
-    private String descripcion;
-
-    @Column
     private double precio;
 
     @Column
@@ -46,14 +43,6 @@ public class Producto {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
     }
 
     public double getPrecio() {
