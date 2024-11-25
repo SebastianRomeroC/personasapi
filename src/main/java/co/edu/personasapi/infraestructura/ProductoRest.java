0d4350cff1ac68af39e043590fa92f525c2a03b9
Iván Piñeros <ipineros@uniempresarial.edu.co>
2024-11-25 12:32:34 -0500

    Update ProductoRest.java

--- src/main/java/co/edu/personasapi/infraestructura/ProductoRest.java
+++ src/main/java/co/edu/personasapi/infraestructura/ProductoRest.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
  
 import java.util.List;
- 
+ //dbwajbd
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/productosrest")
